f552674c9e5560c47129c4e5eca25fd7baaa9963
Monet <73679634+gerhea@users.noreply.github.com>
2021-10-29 16:14:50 -0400

    repository

---
+++ src/main/java/edu/odu/cs/cs350/DupDetector.java
@@ -0,0 +1,19 @@
+package edu.odu.cs.cs350;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class DupDetector {
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("hello world");
+    }
+
+
+}
