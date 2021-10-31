4c5a410374a82ffbc6f75cc8bc8b96fc6b83ed9f
Andrew <alee035@odu.edu>
2021-10-30 20:30:38 -0400

    UnitTest CommandLine

---
+++ src/test/java/edu/odu/cs/cs350/UnitTestCommandLine.java
@@ -0,0 +1,23 @@
+package edu.odu.cs.cs350;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/*
+ * @author cs_alee035
+ * 
+ */
+
+public class UnitTestCommandLine {
+	
+	@Test
+	public void testFindSourceFiles() {
+		
+	}
+}
